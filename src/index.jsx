c9eaaabdb8cd05a99b26d5e16bff821ce3038a66
chiarazetaa <chiara.zuffi19@gmail.com>
2025-02-16 18:57:09 +0100

    add custom elements

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,24 @@ import reactLogo from './react-logo.png';
 
 const root = createRoot(document.getElementById('root'))
 
+function Header() {
+    return (
+        <header className='header'>
+            <img src={reactLogo} alt="React logo" />
+            <nav>
+                <ul className='nav-list'>
+                    <li className='nav-list-item'>Pricing</li>
+                    <li className='nav-list-item'>About</li>
+                    <li className='nav-list-item'>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
 function Main() {
     return (
-        <main>
-            <img src={reactLogo} alt="React logo" />
+        <>
             <h1>Fun facts about react</h1>
             <ul>
                 <li>Was first released in 2013</li>
@@ -15,12 +29,24 @@ function Main() {
                 <li>Is maintained by Meta</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </main>
+        </>
+    )
+}
+
+function Footer() {
+    return(
+        <footer className='footer'>
+            <small>© 2025 chiarazetaa development. All rights reserved.</small>
+        </footer>
     )
 }
 
 root.render(
-    <Main />
+    <>
+        <Header />
+        <Main />
+        <Footer />
+    </>
 )
 
 
