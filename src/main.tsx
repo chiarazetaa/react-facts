ab80d0009871d7782c038c369c829bbe1ced94d6
chiarazetaa <chiara.zuffi19@gmail.com>
2025-02-16 10:44:30 +0100

    create element

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
+import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById('root')!)
-root.render(<h1>Hello React!!</h1>)
+const root = createRoot(document.getElementById('root')!) // ! = it won't be null
+root.render(
+    createElement('h1', null, 'Hello from React')
+)
