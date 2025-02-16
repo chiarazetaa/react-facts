bae0ce5aff5093b13a6bafb1c4fe79c2007a1ef3
chiarazetaa <chiara.zuffi19@gmail.com>
2025-02-16 14:05:56 +0100

    add custom element

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createRoot } from 'react-dom/client'
+import reactLogo from './react-logo.png';
+
+const root = createRoot(document.getElementById('root')!) // ! = it won't be null
+
+function Main() {
+    return (
+        <main>
+            <img src={reactLogo} alt="React logo" />
+            <h1>Fun facts about react</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+root.render(
+    <Main />
+)
+
+
