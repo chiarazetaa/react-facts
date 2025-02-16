0e11bc85b38e6f96d2f974b247ad2817ca2ae423
chiarazetaa <chiara.zuffi19@gmail.com>
2025-02-16 11:29:38 +0100

    react composability

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
-import { createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root')!) // ! = it won't be null
+
+function MainContent() {
+    return (
+        <h1>JSX yeah!</h1>
+    )
+}
+
 root.render(
-    createElement('h1', null, 'Hello from React')
+    <div>
+        <MainContent></MainContent>
+    </div>
 )
